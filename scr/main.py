b07070849adee838c2e3591e89ace4a9ab020626
Swati Sharma <swatisharma83915@gmail.com>
2026-01-17 15:11:36 +0530

    Add data loading, analysis, and visualization

--- scr/main.py
+++ scr/main.py
@@ -1,1 +1,8 @@
+from data_loader import load_data
+from analysis import calculate_soh
+from visualization import plot_battery_health
+
+data = load_data("../data/battery_data.csv")
+data = calculate_soh(data)
+plot_battery_health(data)
 
