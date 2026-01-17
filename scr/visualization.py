72de6d64da7b672b71452ba0d80f550a1fd7ef5b
Swati Sharma <swatisharma83915@gmail.com>
2026-01-17 15:11:58 +0530

    Add function to plot battery health using matplotlib

--- scr/visualization.py
+++ scr/visualization.py
@@ -1,1 +1,10 @@
+import matplotlib.pyplot as plt
+
+def plot_battery_health(df):
+    plt.plot(df["cycle"], df["SOH (%)"])
+    plt.xlabel("Charge Cycles")
+    plt.ylabel("State of Health (%)")
+    plt.title("EV Battery Health Degradation")
+    plt.grid()
+    plt.show()
 
